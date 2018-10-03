926af27562280959d9cdeb2550c790e3b9ee917a
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-10-03 23:41:32 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'home#index'
+  root 'issue#index'
 
   resources :issues do
-    resources :votes, only: %w(new create)
+    resources :votes, only: :create
   end
 end
