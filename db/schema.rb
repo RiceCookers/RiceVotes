0d5f481cf34d107d060febdda42b1afc863131da
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-10-03 22:50:51 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_03_134944) do
+
+  create_table "issues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
