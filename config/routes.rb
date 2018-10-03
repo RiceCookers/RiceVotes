7ee61176605c33e66c23271818082984be00b3d5
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-10-03 23:12:39 +0900

    add: admin_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admin_users
   root 'home#index'
 
   resources :issues
---
+++ db/migrate/20181003141026_devise_create_admin_users.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class DeviseCreateAdminUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admin_users do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_index :admin_users, :email,                unique: true
+  end
+end
